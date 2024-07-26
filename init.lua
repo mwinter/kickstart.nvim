129738d032e191553729c54531818ce20cf8defd
Mark Winter <mark.winter@epicgames.com>
2024-07-26 12:28:36 +0100

    Force spaces by default

--- init.lua
+++ init.lua
@@ -145,6 +145,9 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', space = '·', trail = '·', nbsp = '␣', eol = '↓' }
 -- vim.opt.binary = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
